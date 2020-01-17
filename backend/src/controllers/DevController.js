9f28d151bf86c4e5535e88b36e079e7178478de3
Diogo Lisboa <diogodow@gmail.com>
2020-01-17 17:03:16 -0300

    websocket added to mobile and backend

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket')
 
 module.exports = {
     async index(request, response){
@@ -12,7 +13,7 @@ module.exports = {
     async store(request, response) {
         const { github_username, techs, latitude, longitude } = request.body;
     
-        const dev = await Dev.findOne({ github_username });
+        let dev = await Dev.findOne({ github_username });
 
         if (!dev){
 
@@ -35,6 +36,13 @@ module.exports = {
             techs: techsArray,
             location
         });
+
+        const sendSocketMessageTo = findConnections(
+            { latitude, longitude },
+            techsArray
+        )
+        
+        sendMessage(sendSocketMessageTo, 'new-dev', dev)
     }
     
         return response.json(dev);
